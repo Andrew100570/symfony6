8d59591d34c50638d0d7aaa969b314ca6bd461b5
Andry <blader_100@mail.ru>
2023-04-03 16:44:11 +0300

    Controller przyjmuje 2 parametry int

---
+++ src/Controller/ExchangeController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\History;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use DateTimeImmutable;
+
+class ExchangeController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    //post
+    #[Route('/exchange/values', name: 'app_exchange_post', methods: ['POST'])]
+    public function postHistory(Request $request): Response
+    {
+
+        $firstIn  = json_decode($request->getContent(), true)['firstIn'];
+        $secondIn = json_decode($request->getContent(), true)['secondIn'];
+
+        $SearchRes = $this->entityManager->createQuery('SELECT h From App\Entity\History h WHERE h.firstIn = '.$firstIn);
+        $results = $SearchRes->getResult();
+
+        foreach ($results as $result) {
+            $currentHistory = $this->entityManager->getRepository(History::class)->find($result->getId());
+            $currentHistory->setFirstIn($secondIn)
+                ->setSecondIn($firstIn)
+                ->setFirstOut($currentHistory->getFirstOut())
+                ->setSecondOut($currentHistory->getSecondOut())
+                ->setDataUtworzenia(($currentHistory->getDataUtworzenia()))
+                ->setDataAktualizacji(new DateTimeImmutable());
+            $this->entityManager->persist($currentHistory);
+            $this->entityManager->flush();
+
+        }
+
+        return JsonResponse::fromJsonString('Wartości secondIn i firstIn są zamieniane');
+    }
+}
